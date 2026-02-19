2ea0f934b3dfe3d10a93451ac00ef9b3570d9c8a
Andrew Wray <drew.wray@gmail.com>
2026-02-19 16:04:13 -0700

    a

--- src/common/autoloads/game_constants.gd
+++ src/common/autoloads/game_constants.gd
@@ -3,12 +3,17 @@ class_name GameConstants
 const PLAYER_ANIM_LIB =         "player_animation_library/"
 
 const ANIMS = {
-	"PLAYER_ANIM_IDLE_UNARMED": PLAYER_ANIM_LIB + "idle_unarmed",
-	"PLAYER_ANIM_RUN_UNARMED":  PLAYER_ANIM_LIB + "run_unarmed",
-	"PLAYER_ANIM_JUMP":         PLAYER_ANIM_LIB + "jump",
-	"PLAYER_ANIM_FALL":         PLAYER_ANIM_LIB + "fall",
-	"PLAYER_ANIM_SOMERSAULT":   PLAYER_ANIM_LIB + "somersault",
-	"PLAYER_ANIM_ATTACK1":      PLAYER_ANIM_LIB + "attack1",
-	"PLAYER_ANIM_ATTACK2":      PLAYER_ANIM_LIB + "attack2",
-	"PLAYER_ANIM_ATTACK3":      PLAYER_ANIM_LIB + "attack3"
+	"PLAYER_ANIM_IDLE_UNARMED": 		PLAYER_ANIM_LIB + "idle_unarmed",
+	"PLAYER_ANIM_RUN_UNARMED":  		PLAYER_ANIM_LIB + "run_unarmed",
+	"PLAYER_ANIM_JUMP":         		PLAYER_ANIM_LIB + "jump",
+	"PLAYER_ANIM_FALL":         		PLAYER_ANIM_LIB + "fall",
+	"PLAYER_ANIM_SOMERSAULT":   		PLAYER_ANIM_LIB + "somersault",
+	"PLAYER_ANIM_ATTACK1":      		PLAYER_ANIM_LIB + "attack1",
+	"PLAYER_ANIM_ATTACK2":      		PLAYER_ANIM_LIB + "attack2",
+	"PLAYER_ANIM_ATTACK3":      		PLAYER_ANIM_LIB + "attack3",
+	"PLAYER_ANIM_AIR_ATTACK1":  		PLAYER_ANIM_LIB + "air_attack1",
+	"PLAYER_ANIM_AIR_ATTACK2":  		PLAYER_ANIM_LIB + "air_attack2",
+	"PLAYER_ANIM_AIR_ATTACK3_READY":  	PLAYER_ANIM_LIB + "air_attack3_ready",
+	"PLAYER_ANIM_AIR_ATTACK3_LOOP":   	PLAYER_ANIM_LIB + "air_attack3_loop",
+	"PLAYER_ANIM_AIR_ATTACK3_END":    	PLAYER_ANIM_LIB + "air_attack3_end"
 }
